354fff08c4854e416b72c62acc67882c579a1f8c
beom-cheol kim <babybeom@gmail.com>
2017-12-14 11:51:47 +0900

    Initial Commit

---
+++ bububaba/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bububaba
+//
+//  Created by kimbeom-cheol on 2017. 12. 14..
+//  Copyright © 2017년 kimbeom-cheol. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
